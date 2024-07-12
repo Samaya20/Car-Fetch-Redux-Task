1b5a88032c49480f4ef057a887f5ccb8d426db65
Samaya Mughumova <81756158+Samaya20@users.noreply.github.com>
2024-07-12 15:30:48 +0400

    Update CarItem.jsx

--- src/components/CarItem.jsx
+++ src/components/CarItem.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
-const CarItem = ({car}) => {
-  return <div>
-    <h1>{car.model}</h1>
-  </div>;
+const CarItem = ({ car }) => {
+  return (
+    <li>
+      <img src={car.imageUrl} alt="carimage"></img>
+      <div className="text">
+        <p>
+          <span>Model : </span>
+          {car.model}
+        </p>
+        <p>
+          <span>Vendor: </span>
+          {car.vendor}
+        </p>
+        <p>
+          <span>Price: </span>
+          {car.price}
+        </p>
+      </div>
+    </li>
+  );
 };
 
 export default CarItem;
